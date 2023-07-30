697b91933d5666b46033600636b4ed69b525ec2d
piyushkolhe869869 <kolhepiyush222@gmail.com>
2023-07-30 19:56:57 +0530

    piyushkolhe

--- Inmmutableclass/src/inmmutableclassh/imuutableee.java
+++ Inmmutableclass/src/inmmutableclassh/imuutableee.java
@@ -2,26 +2,33 @@ package inmmutableclassh;
 
 public class imuutableee {
 
-	private final int id;
-	
-	private final String name;
-	
-	
-	public imuutableee(int id,String name) {
-		
-		this.id=id;
-		this.name=name;
-	}
+private final int id;
+
+private final String name;
+
+public imuutableee(int id,String name){
+
+	 this.id=id;
+	 this.name=name;
+  
+      System.out.println("piyush edited");
+
+}
+
+public int getId() {
+	return id;
+}
+
+public String getName() {
+	return name;
+}
 
 
-	public int getId() {
-		return id;
-	}
 
 
-	public String getName() {
-		return name;
-	}
+
+
+
 	
 	
 }
